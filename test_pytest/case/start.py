7609a2e9147cc9761cf06d1b4f5965ff9bc364d5
471477005 <471477005@qq.com>
2020-07-31 17:20:05 +0800

    修改路径

--- test_pytest/case/start.py
+++ test_pytest/case/start.py
@@ -1,6 +1,6 @@
 import os
 
-os.chdir('/var/jenkins_home/workspace/test1/test_pytest')
+os.chdir('/var/jenkins_home/workspace/test_01/test_pytest')
 print(os.getcwd())
 os.system('pytest --alluredir ./report/allure_raw --clean-alluredir')
 os.system('allure serve report/allure_raw')
