6249f8612ecda101c67cc7e238cb47c170b6a85b
471477005 <471477005@qq.com>
2020-08-05 10:56:57 +0800

    更新 python -m pytest   Python下启动 pytest

--- test_pytest/case/start.py
+++ test_pytest/case/start.py
@@ -2,6 +2,7 @@ import os
 
 os.chdir('/var/jenkins_home/workspace/test_01/test_pytest')
 print(os.getcwd())
-os.system('pytest --alluredir ./report/allure_raw --clean-alluredir')
-os.system('allure serve report/allure_raw')
-
+os.system('python -m pytest --alluredir ./report/allure_raw --clean-alluredir')
+print('pytest start')
+os.system('python -m allure serve report/allure_raw')
+print('allure start')
