30505ea95f8c617cd3abf42aa801a38e5963700e
471477005 <471477005@qq.com>
2020-08-07 09:38:35 +0800

    最大化运行（全屏窗口）设置元素定位比较准确
    chrome_options.add_argument('--start-maximized') # 最大化运行（全屏窗口）设置元素定位比较准确

--- test_pytest/案件/conftest.py
+++ test_pytest/案件/conftest.py
@@ -11,6 +11,7 @@ def driver(request):
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--start-maximized') # 最大化运行（全屏窗口）设置元素定位比较准确
     _driver = webdriver.Chrome(options=chrome_options)
     _driver.maximize_window()  # 最大化
     print(_driver.title)
